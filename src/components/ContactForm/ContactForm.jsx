f0f1bd897a265eaa737f15d8cec2d056b9e69a5a
David <javidoru02@gmail.com>
2026-07-07 02:48:29 +0300

    style form and btn

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./ContactForm.css";
 
 class ContactForm extends Component {
   state = {
@@ -13,7 +14,7 @@ class ContactForm extends Component {
       [event.target.name]: event.target.value,
     });
   };
-  
+
   hendleSubmit = (event) => {
     event.preventDefault();
     const { firstName, lastName, email, phone } = this.state;
@@ -30,49 +31,93 @@ class ContactForm extends Component {
       email: "",
       phone: "",
     });
-  }
+  };
 
   render() {
     return (
       <div>
-        <form className="ccreate-contact" onSubmit={this.hendleSubmit}>
-          <input
-            type="text"
-            name="firstName"
-            placeholder="FirstName"
-            value={this.state.firstName}
-            onChange={this.hendleChange}
-          />
-          <input
-            type="text"
-            name="lastName"
-            placeholder="LastName"
-            value={this.state.lastName}
-            onChange={this.hendleChange}
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={this.hendleChange}
-          />
-          <input
-            type="tel"
-            name="phone"
-            placeholder="Phone"
-            value={this.state.phone}
-            onChange={this.hendleChange}
-          />
+        <form className="create-contact" onSubmit={this.hendleSubmit}>
+          <div className="input-container">
+            <input
+              type="text"
+              name="firstName"
+              placeholder="FirstName"
+              value={this.state.firstName}
+              onChange={this.hendleChange}
+            />
+            <button
+              type="button"
+              className="clear-btn"
+              onClick={() => this.setState({ firstName: "" })}
+            >
+              x
+            </button>
+          </div>
 
-          <button className="btn-add" onClick={this.hendleSubmit}>
-            Add Contact
-          </button>
+          <div className="input-container">
+            <input
+              type="text"
+              name="lastName"
+              placeholder="LastName"
+              value={this.state.lastName}
+              onChange={this.hendleChange}
+            />
+            <button
+              type="button"
+              className="clear-btn"
+              onClick={() => this.setState({ lastName: "" })}
+            >
+              x
+            </button>
+          </div>
 
-          <button className="btn-del">
-            Delete Contact
-          </button>
+          <div className="input-container">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.hendleChange}
+            />
+            <button
+              type="button"
+              className="clear-btn"
+              onClick={() => this.setState({ email: "" })}
+            >
+              x
+            </button>
+          </div>
+
+          <div className="input-container">
+            <input
+              type="tel"
+              name="phone"
+              placeholder="Phone"
+              value={this.state.phone}
+              onChange={this.hendleChange}
+            />
+            <button
+              type="button"
+              className="clear-btn"
+              onClick={() => this.setState({ phone: "" })}
+            >
+              x
+            </button>
+          </div>
         </form>
+
+        <div className="btn-holder">
+          <div className="left-btn">
+            <button className="btn-new">New</button>
+          </div>
+          <div className="right-btn">
+            <button className="btn-add" onClick={this.hendleSubmit}>
+              Add
+            </button>
+
+            <button className="btn-del">Delete</button>
+          </div>
+        </div>
       </div>
     );
   }
